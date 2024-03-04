d26a730e3d380dbe941aebf5121f374c50797a3b
Thijs <thijsvanrijsbergen@gmail.com>
2024-03-04 14:54:05 +0100

    Update project URL and add constructor to Scm class

--- src/test/java/com/avansdevops/SprintTest.java
+++ src/test/java/com/avansdevops/SprintTest.java
@@ -9,96 +9,97 @@ import java.util.Date;
 import org.junit.Test;
 
 public class SprintTest {
-    
-    //requirement 2
+
+    // requirement 2
     @Test
     public void projectCanMakeSprint() {
         Project project = new Project("Project 1");
-        //create dates
+        // create dates
         Calendar calendar = Calendar.getInstance();
         calendar.set(2024, 10, 10);
         Date startDate = calendar.getTime();
         calendar.set(2024, 10, 20);
         Date endDate = calendar.getTime();
 
-        //add the sprint
-        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT, project.getBacklog());
+        // add the sprint
+        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT,
+                project.getBacklog());
         project.addSprint(sprint);
         assertNotNull(project.getSprints());
     }
 
     @Test
-    public void sprintCantBeEditedAfterStart() 
-    {
+    public void sprintCantBeEditedAfterStart() {
         Project project = new Project("Project 1");
-        //create dates
+        // create dates
         Calendar calendar = Calendar.getInstance();
         calendar.set(2024, 10, 10);
         Date startDate = calendar.getTime();
         calendar.set(2024, 10, 20);
         Date endDate = calendar.getTime();
 
-        //add the sprint
-        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT, project.getBacklog());
+        // add the sprint
+        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT,
+                project.getBacklog());
         project.addSprint(sprint);
         sprint.start();
         try {
             sprint.setName("Anti jumping feature");
         } catch (IllegalStateException e) {
-            assertEquals(e.getMessage(), "Sprint already started");
+            assertEquals("Sprint already started", e.getMessage());
         }
-        assertEquals(sprint.getName(), "Anti flickering feature");
+        assertEquals("Anti flickering feature", sprint.getName());
     }
 
     @Test
-    public void sprintCanBeEditedBeforeStart()
-    {
+    public void sprintCanBeEditedBeforeStart() {
         Project project = new Project("Project 1");
-        //create dates
+        // create dates
         Calendar calendar = Calendar.getInstance();
         calendar.set(2024, 10, 10);
         Date startDate = calendar.getTime();
         calendar.set(2024, 10, 20);
         Date endDate = calendar.getTime();
 
-        //add the sprint
-        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT, project.getBacklog());
+        // add the sprint
+        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT,
+                project.getBacklog());
         project.addSprint(sprint);
         sprint.setName("Anti jumping feature");
-        assertEquals(sprint.getName(), "Anti jumping feature");
+        assertEquals("Anti jumping feature", sprint.getName());
     }
 
     @Test
-    public void sprintIsAutoStartedWhenStartDateIsInPastOrToday()
-    {
+    public void sprintIsAutoStartedWhenStartDateIsInPastOrToday() {
         Project project = new Project("Project 1");
-        //create dates
+        // create dates
         Calendar calendar = Calendar.getInstance();
         calendar.set(2018, 10, 10);
         Date startDate = calendar.getTime();
         calendar.set(2018, 10, 20);
         Date endDate = calendar.getTime();
 
-        //add the sprint
-        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT, project.getBacklog());
+        // add the sprint
+        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT,
+                project.getBacklog());
         project.addSprint(sprint);
-        assertEquals(sprint.isStarted(), true);
+        assertEquals(true, sprint.isStarted());
     }
 
     @Test
-    public void sprintIsNotAutoStartedWhenStartDateIsInFuture()
-    {
+    public void sprintIsNotAutoStartedWhenStartDateIsInFuture() {
         Project project = new Project("Project 1");
-        //create dates
+        // create dates
         Calendar calendar = Calendar.getInstance();
         calendar.set(2024, 10, 10);
         Date startDate = calendar.getTime();
         calendar.set(2024, 10, 20);
         Date endDate = calendar.getTime();
 
-        //add the sprint
-        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT, project.getBacklog());
+        // add the sprint
+        Sprint sprint = new Sprint("Anti flickering feature", startDate, endDate, SprintGoal.PARTIALPRODUCT,
+                project.getBacklog());
         project.addSprint(sprint);
-        assertEquals(sprint.isStarted(), false);
+        assertEquals(false, sprint.isStarted());
     }
 }
